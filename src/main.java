06088b8108479b7c2354ec18d748fee689ef2dc2
Fred <frederico.d.silva@outlook.com>
2019-11-10 23:26:14 -0300

    inserts

--- src/main.java
+++ src/main.java
@@ -10,6 +10,7 @@ public class main {
 
 		int contaIf = 0;
 		int contaFor = 0;
+		int contaTroca = 0;
 
 		int quantidade = 1000; // QUANTIDADE DE REGISTROS
 
@@ -46,21 +47,37 @@ public class main {
 //		INSERTIONSORT
 		int j;
 		int key;
+		vet = new int[quantidade];
+		for (i = 0; i < vet.length; i++) {
+			vet[i] = (int) (Math.random() * quantidade);
 
+		}
 		// TEMPO INICIAL E FINAL PARA MEDIR O TEMPO DE EXECUÇÃO
 		tempoInicial = System.currentTimeMillis();
 
+		contaIf = 0;
+		contaFor = 0;
 		for (j = 1; j < vet.length; j++) {
 			key = vet[j];
+			contaFor++;
 			for (i = j - 1; (i >= 0) && (vet[i] > key); i--) {
 				vet[i + 1] = vet[i];
+				contaTroca++;
 			}
 			vet[i + 1] = key;
+
 		}
 
 		tempoFinal = System.currentTimeMillis();
-		System.out.println("INSERTIONSORT Executado em = " + (tempoFinal - tempoInicial) + " ms");
+		System.out.println("Quantidade de interações: " + contaFor + " " + "Quantidade de comparacoes: " + contaIf + " "
+				+ "Quantidade de permutas " + contaTroca + " " + "INSERTION SORT Executado em = "
+				+ (tempoFinal - tempoInicial) + " ms");
 
+		vet = new int[quantidade];
+		for (i = 0; i < vet.length; i++) {
+			vet[i] = (int) (Math.random() * quantidade);
+
+		}
 //		QUICKSORT
 		tempoInicial = System.currentTimeMillis();
 
